ffbe8a42a8da9366c97146fca93e7eceeab37c79
Maximilian Hahn <maximilian.hahn@datagroup.de>
2026-05-07 12:47:11 +0000

    Initial commit

---
+++ webapp/controller/View1.controller.ts
@@ -0,0 +1,11 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace com.sapwebshop2026.sapwebshop.controller
+ */
+export default class View1 extends Controller {
+
+    public onInit(): void {
+
+    }
+}
